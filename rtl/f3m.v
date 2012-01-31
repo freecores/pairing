361bfac9623779c3533493788bcab62bc6d998c4
homer.xing <homer.xing@72179f5f-4300-4bd0-916a-fde7b2809594>
2012-01-31 09:14:07 +0000

    use logic for $f3m_mux6$

--- rtl/f3m.v
+++ rtl/f3m.v
@@ -19,17 +19,14 @@ endmodule
 module f3m_mux6(v0, v1, v2, v3, v4, v5, l0, l1, l2, l3, l4, l5, out);
     input l0, l1, l2, l3, l4, l5;
     input [`WIDTH:0] v0, v1, v2, v3, v4, v5;
-    output reg [`WIDTH:0] out;
-    always @ (l0,l1,l2,l3,l4,l5,v0,v1,v2,v3,v4,v5)
-      case ({l0,l1,l2,l3,l4,l5})
-        6'b100000: out = v0;
-        6'b010000: out = v1;
-        6'b001000: out = v2;
-        6'b000100: out = v3;
-        6'b000010: out = v4;
-        6'b000001: out = v5;
-        default: out = 0;
-      endcase
+    output [`WIDTH:0] out;
+    genvar i;
+    generate
+        for(i=0;i<=`WIDTH;i=i+1)
+          begin : label
+            assign out[i] = (v0[i]&l0)|(v1[i]&l1)|(v2[i]&l2)|(v3[i]&l3)|(v4[i]&l4)|(v5[i]&l5);
+          end 
+    endgenerate
 endmodule
 
 // f3m_add: C = A + B, in field F_{3^M}
